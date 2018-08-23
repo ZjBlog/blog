ef2f3d9391df5c221b3999bef217580e9b6ae0a5
zhangjunTracy <13552960783@163.com>
2018-08-23 19:22:19 +0800

    es6配置

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import 'babel-polyfill'
+// import 'babel-polyfill'
+// 在入口处引入了 就不需要再引入了
 import Vue from 'vue'
 import App from './App'
 import router from './router'
